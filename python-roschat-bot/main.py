243a045ec47f67f50839ccc434c0865eca517091
Vladislav <vladislav_dorofeev@epam.com>
2025-06-06 23:49:29 +0400

    Refactors event handling in SocketHandler
    Simplifies event handling by removing redundant callback and handler dictionaries.
    
    Introduces `register_callback` and `register_handler` methods to streamline event registration and emission.
    
    Improves code clarity and maintainability by consolidating event-related logic within the SocketHandler class.

--- python-roschat-bot/main.py
+++ python-roschat-bot/main.py
@@ -57,8 +57,6 @@ class SocketHandler(socketio.ClientNamespace):
                                     engineio_logger=logger if debug_mode else debug_mode)
 
         self._sio.register_namespace(self)
-        self._callbacks = {event.value: self.default_callback for event in ServerEvents}
-        self._handlers = {event.value: self.default_callback for event in ServerEvents}
 
     def on_connect(self) -> None:
         ...
@@ -100,7 +98,7 @@ class SocketHandler(socketio.ClientNamespace):
         event = ServerEvents.BOT_MESSAGE_WATCHED.value
         self._sio.emit(event, data={'id': msg_id}, callback=self._callbacks.get(event))
 
-    def message_delete(self, msg_id: int):
+    def message_delete(self, msg_id: int) -> None:
         event = ServerEvents.DELETE_BOT_MESSAGE.value
         self._sio.emit(event, data={'id': msg_id}, callback=self._callbacks.get(event))
 
@@ -111,20 +109,14 @@ class SocketHandler(socketio.ClientNamespace):
         event = ServerEvents.SET_BOT_KEYBOARD.value
         self._sio.emit(event, data=data, callback=self._callbacks.get(event))
 
-    def handler_registration(self, event: ServerEvents, handler: Callable):
+    def register_callback(self, data: dict, event: ServerEvents, callback: Callable) -> None:
+        if not data.get('cid'):
+            raise ValueError("Required the cid field is not provided")
+        self._sio.emit(event, data=data, callback=callback)
+
+    def register_handler(self, event: ServerEvents, handler: Callable) -> None:
         wrapped_handler = functools.partial(handler, event=event, socket_handler=self)
         self._sio.on(event, handler=wrapped_handler)
-
-    # def processing_message_event(self) -> None:
-    #     event = ServerEvents.BOT_MESSAGE_EVENT.value
-    #     wrapped = functools.partial(self._handlers.get(event), socket_handler=self)
-    #     self._sio.on(event, handler = wrapped)
-    #
-    #
-    # def processing_button_event(self) -> None:
-    #     event = ServerEvents.BOT_BUTTON_EVENT.value
-    #     wrapped = functools.partial(self._handlers.get(event), socket_handler=self)
-    #     self._sio.on(event, handler = wrapped)
 
     def wait(self) -> None:
         self._sio.wait()
@@ -170,11 +162,11 @@ class RosChatBot:
         return f"{self._settings.base_url}:{web_sockets_port}"
 
     def add_callback(self, event: ServerEvents, callback: Callable) -> None:
-        self.socket_handler._callbacks[event.value] = callback
+        self.socket_handler.register_callback()
         self.logger.info(f"Callback for {event} was changed")
 
     def add_handler(self, event: ServerEvents, handler: Callable) -> None:
-        self.socket_handler.handler_registration(event, self.server_outcome_unpacking_decorator(handler))
+        self.socket_handler.register_handler(event, self.server_outcome_unpacking_decorator(handler))
         self.logger.info(f"Handler for {event} was added")
 
     def run_polling(self):
